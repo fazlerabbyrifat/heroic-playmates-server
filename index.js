bcb09370a86e67ebbe32a7ed8be3e21d2ae5b46a
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-05-21 13:46:51 +0600

    get data using email implemented

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(express.json());
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_USER_PASSWORD;
 
-const uri =
-  `mongodb+srv://${username}:${password}@cluster0.uz4gpo0.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${username}:${password}@cluster0.uz4gpo0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,38 +24,61 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const heroicCollection = client
+      .db("heroicPlaymatesDB")
+      .collection("categories");
+    const trendingCollection = client
+      .db("heroicPlaymatesDB")
+      .collection("trending");
+    const allToysCollection = client
+      .db("heroicPlaymatesDB")
+      .collection("allToys");
 
-    const heroicCollection = client.db("heroicPlaymatesDB").collection("categories");
-    const trendingCollection = client.db("heroicPlaymatesDB").collection('trending');
-    const allToysCollection = client.db("heroicPlaymatesDB").collection('allToys');
-
-    app.get('/categories', async(req, res) => {
-        const result = await heroicCollection.find().toArray();
-        res.send(result);
+    app.get("/categories", async (req, res) => {
+      const result = await heroicCollection.find().toArray();
+      res.send(result);
     });
 
-    app.get('/trending', async( req, res ) => {
-        const result = await trendingCollection.find().toArray();
-        res.send(result);
+    app.get("/trending", async (req, res) => {
+      const result = await trendingCollection.find().toArray();
+      res.send(result);
     });
 
-    app.get('/allToys', async (req, res) => {
-        const result = await allToysCollection.find().limit(20).toArray();
-        res.send(result);
+    app.get("/allToys", async (req, res) => {
+      const { search, page } = req.query;
+      const query = search ? { name: { $regex: search, $options: "i" } } : {};
+      const limit = 20;
+      const skip = page ? (parseInt(page) - 1) * limit : 0;
+      const result = await allToysCollection
+        .find(query)
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+      res.send(result);
     });
-
-    app.get("/allToys/:id", async( req, res ) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await allToysCollection.findOne(query);
+ 
+    app.get("/allToys/:email", async(req, res) => {
+        const email = req.params.email;
+        
+        const query = { email: email };
+        // let result;
+        
+        const result = await allToysCollection.find(query).toArray();
         res.send(result);
     })
 
-    app.post('/allToys', async( req, res ) => {
-        const newToy = req.body;
-        const result = await allToysCollection.insertOne(newToy);
-        res.send(result);
-    })
+    app.get("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allToysCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/allToys", async (req, res) => {
+      const newToy = req.body;
+      const result = await allToysCollection.insertOne(newToy);
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
